0bbe224a60222df9a639275637cecb8b63ce0941
JDamianDelgado <joakodelgado2013@gmail.com>
2026-02-13 22:47:28 -0300

    ....

--- src/config/diccionarioVariables.js
+++ src/config/diccionarioVariables.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-dotenv.config();
-
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 export const DICCIONARIO_VAR = {
   MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
   PORT: process.env.PORT,
